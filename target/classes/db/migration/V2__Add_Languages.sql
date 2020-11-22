9bdfd37fbcafd40500f9f8ea2d9a6b26cecf38da
Ilowski <wojtek-ilowski@wp.pl>
2020-11-22 18:20:08 +0000

    xx

---
+++ target/classes/db/migration/V2__Add_Languages.sql
@@ -0,0 +1,3 @@
+insert into languages (welcomeMessage, codeLanguage) values ('Hello','en');
+insert into languages (welcomeMessage, codeLanguage) values ('Witam','pl');
+insert into languages (welcomeMessage, codeLanguage) values ('Hallo','de');
