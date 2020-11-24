43056611919c85c898828553651a5267441ee435
Ilowski <wojtek-ilowski@wp.pl>
2020-11-24 15:13:30 +0000

    removed target

--- target/classes/db/migration/V1__Create_language_table.sql
+++
@@ -1,5 +0,0 @@
-create table languages (
- id int unsigned PRIMARY KEY AUTO_INCREMENT,
-welcomemessage VARCHAR(150) NOT NULL,
-codeLanguage VARCHAR(3)
-);
--- target/classes/db/migration/V2__Add_Languages.sql
+++
@@ -1,3 +0,0 @@
-insert into languages (welcomeMessage, codeLanguage) values ('Hello','en');
-insert into languages (welcomeMessage, codeLanguage) values ('Witam','pl');
-insert into languages (welcomeMessage, codeLanguage) values ('Hallo','de');
