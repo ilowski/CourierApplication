ad7b8fd2e05b959082849e970aa2063fc4f8c47b
Ilowski <wojtek-ilowski@wp.pl>
2020-11-25 22:07:00 +0000

    refactor frontend - separation into separate files

---
+++ src/main/webapp/scripts/script.js
@@ -0,0 +1,39 @@
+const API_URL = 'http://localhost:8080/api';
+    const CODE_TO_EMOJI = {
+    'en': 'england.png',
+    'pl': 'poland.png',
+    'de': 'german.png'
+    };
+
+    fetch(`${API_URL}/langs`)
+    .then(response => response.json())
+    .then((languages) => {
+    const checkboxes = languages.map(l => `
+    <div class="form-group">
+    <label class="pure-radio">
+      <input type="radio" name="lang" value="${l.id}">
+      <img src="../images/${CODE_TO_EMOJI[l.codeLanguage]}"</img>
+    </label>
+    </div>
+    `).join('\n');
+    document.getElementById('langs').innerHTML = checkboxes;
+    });
+    const div = document.getElementById('welcome');
+    const form = document.getElementById('welcomeForm');
+
+    document.getElementById('btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    const formObj = {
+    name: form.elements.name.value,
+    lang: form.elements.lang.value
+    };
+    fetch (`${API_URL}?${new URLSearchParams(formObj)}`)
+      .then(response => response.text())
+      .then((text) => {
+       div.innerHTML = `
+       <div class="row">
+       <h1>${text}</h1>
+       </div>
+       `;
+       });
+});
