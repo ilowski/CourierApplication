baa4c64f5c4da939ac9a4d904630d210f45aa684
Ilowski <wojtek-ilowski@wp.pl>
2020-12-08 20:13:46 +0000

    initialize tipService, change method doGet to return values from method tipService.summaryOfTips

--- src/main/java/servlet/TipServlet.java
+++ src/main/java/servlet/TipServlet.java
@@ -6,6 +6,7 @@ import database.TipRepository;
 import entity.Tip;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import service.TipService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,23 +19,26 @@ import java.io.IOException;
 public class TipServlet extends HttpServlet  {
     private TipRepository tipRepository;
     private ObjectMapper objectMapper;
+    private TipService tipService;
     private final Logger logger = LoggerFactory.getLogger(TipServlet.class);
     @SuppressWarnings("unused")
     public TipServlet() {
-        this(new TipRepository(), new ObjectMapper());
+        this(new TipRepository(), new ObjectMapper(), new TipService());
     }
 
-    public TipServlet(TipRepository tipRepository, ObjectMapper objectMapper) {
+    public TipServlet(TipRepository tipRepository, ObjectMapper objectMapper, TipService tipService) {
         this.tipRepository = tipRepository;
         this.objectMapper = objectMapper;
+        this.tipService = tipService;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        logger.info("Request got on TipServlet");
-        resp.setContentType("application/json;charset=UTF-8");
-        objectMapper.writeValue(resp.getOutputStream(), tipRepository.findAllTips());
+            logger.info("Request got on TipServlet");
+            resp.setContentType("application/json;charset=UTF-8");
+            objectMapper.writeValue(resp.getOutputStream(), tipService.summaryOfTips());
+    
 
     }
 
