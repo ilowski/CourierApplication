dc353e4fba41f31f68300fe15912929ba3c8e5eb
Ilowski <wojtek-ilowski@wp.pl>
2020-12-08 20:15:50 +0000

    change logic of prepareTipMessage method - remove nickName from return. Add method summaryOfTips and use it prepareSummaryTipMessage method

--- src/main/java/service/TipService.java
+++ src/main/java/service/TipService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 public class TipService {
     TipRepository tipRepository;
+
     Logger logger = LoggerFactory.getLogger(TipService.class);
 
     public TipService() {this(new TipRepository());}
@@ -18,7 +19,7 @@ public class TipService {
         this.tipRepository = tipRepository;
     }
 
-    public String prepareTipMessage(String nickName, Float value) {
+    public String prepareTipMessage(String tipMessage, Float value) {
        Float valueForMessage;
        if (value != null) {
            valueForMessage = value;
@@ -26,22 +27,26 @@ public class TipService {
            valueForMessage = 0.0f;
        }
         if (valueForMessage >= 0){
-        String tipMessage = Optional.ofNullable(nickName).orElse("Unnamed") + " thank you for " + valueForMessage  + "$ tip!";
-        return tipMessage;}
+        String tipMessageReturn =  valueForMessage  + "$ tip: " + Optional.ofNullable(tipMessage).orElse("Thank you for it!");
+        return tipMessageReturn;}
         else {
             return "You can't give me negative tip";
         }
 
     }
 
-    public String prepareSummaryTipMessage () {
+    public String summaryOfTips() {
         Float summary = 0f;
         List<Tip> listAllTips = tipRepository.findAllTips();
         for (Tip x : listAllTips) {
             summary += x.getValue();
         }
-        String summaryTipMessage = "Current balance of tips: " + summary;
+       return summary.toString();
+    }
 
+    public String prepareSummaryTipMessage () {
+       String summary = summaryOfTips();
+        String summaryTipMessage = "Current balance of tips: " + summary;
         return summaryTipMessage;
     }
 }
